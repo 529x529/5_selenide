f68f15ea01f018c44bdc0b8ddf2545b3d5ac36ee
mikhailrezchikov <mrezchikov529@gmail.com>
2023-06-06 14:51:53 +0300

    fix

--- src/test/java/github/GithubSolutionTest.java
+++ src/test/java/github/GithubSolutionTest.java
@@ -1,8 +1,5 @@
 package github;
 
-
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
